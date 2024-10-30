415214a056d3c3bd2c810fab02cef94428781a3f
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-10-30 16:39:52 -0500

    arthmetic operators

---
+++ arithmetic-operators.js
@@ -0,0 +1,30 @@
+// Ttpe integer or decimal
+const integer = 43
+const decimal = 43.1
+console.log(typeof decimal, typeof integer)
+
+// Notation scientific
+const scientific = 5e3
+console.log(typeof scientific)
+
+// Infinite y NaN
+const infinite = Infinity
+const NanNumber = NaN
+console.log(typeof infinite, typeof NanNumber)
+
+// Arithmetic operations
+
+// addition, subtraction, multiplication, division
+// + - * /
+
+// module, exponentiation
+// %, **
+
+// precision
+const result = 0.1 + 0.2
+console.log(result.toFixed(1))
+
+// Advanced operations
+const squareRoot = Math.sqrt(16)
+const absoluteValue = Math.abs(-1)
+const random = Math.random()
