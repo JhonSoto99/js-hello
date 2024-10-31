f1bb06ce1fe25819c141e8b0fa64ad96b9c42098
JhonSoto99 <jofeso-bamba@hotmail.com>
2024-10-31 09:22:40 -0500

    local and comparison operators.

---
+++ operators.js
@@ -0,0 +1,11 @@
+// Comparison operators
+
+// equal or different in value ==, !=
+// equal or different in value and date type ===, !==
+// greater than or greater equal than > >=, less than or less equal than  < <=
+
+// Logical operators
+
+// and &&
+// or ||
+// not !
